b04684b8b826b387f79f2d73358305eab0778907
Taranpreet Singh Rayat <taranpreet008@yahoo.in>
2019-09-17 12:37:35 +0530

    initial upload

---
+++ APP/Controllers/Home.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace APP\Controllers;
+
+use APP\Helpers\IP as IP;
+use APP\Core\Redirect as Redirect;
+
+class Home extends \APP\Core\Controller {
+
+   public function index()
+   {
+       echo "MVC 1.0";
+   }
+
+}
---
+++ APP/config/Config.php
@@ -0,0 +1,23 @@
+<?php
+
+//Db Params
+define('DB_HOST','localhost');
+define('DB_DB',''); // enter database name
+define('DB_USER',''); // enter your db user.
+define('DB_PASS','');//enter your database password.
+
+//APP Config
+define('APP_ROOT',dirname(dirname(__FILE__)));
+define('ROOT',dirname(dirname(dirname(__FILE__))));
+define('PUBLIC_ROOT',dirname(dirname(dirname(__FILE__)))."/public");
+define('URL_ROOT',"http://");
+define('SITE_NAME','MVC 1.0');
+define('USE_SSL',true);
+
+define('DEFAULT_CONTROLLER','Home');
+define('DEFAULT_METHOD','index');
+
+//Encryption Key
+define('ENC_KEY','CKXH2U9RPY3EFD70TLS1ZG4N8WQBOVI6AMJ5');
+
+define('TIME_ZONE','Asia/Kolkata');
